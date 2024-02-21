07e6e100064c2e3c9341e9abbce434ccc2d49251
sersim33 <sersim33@gmail.com>
2024-02-21 13:36:39 +0200

    com4

---
+++ test2.py
@@ -0,0 +1,6 @@
+
+numbers = [179, 40, 101]
+
+for number in numbers:
+    last_digit = number % 10
+    print(last_digit)
---
+++ testtest.py
@@ -0,0 +1,19 @@
+vocab = {
+    232: "Alice",
+    550: "Bob",
+    190: "Jack",
+    500: 'all'
+}
+
+for key in vocab:
+    print(f"Hi, {vocab.get(key)}!")
+
+
+
+       
+
+
+
+
+
+

40a20b3b45fb1070f3b7e425165fdeada266a160
sersim33 <sersim33@gmail.com>
2024-02-23 13:27:32 +0200

    com

--- testtest.py
+++ testtest.py
@@ -1,19 +1,15 @@
-vocab = {
-    232: "Alice",
-    550: "Bob",
-    190: "Jack",
-    500: 'all'
-}
+import re
 
-for key in vocab:
-    print(f"Hi, {vocab.get(key)}!")
+def write_employees_to_file(employee_list, path): 
+    with open(path, "w") as fh:
+        for sub_list in employee_list:
+            for employee in sub_list:
+                new_emp = re.sub(r'[0-9,Roe]', '', employee) 
+                fh.write(new_emp + "\n")
 
+    
 
-
-       
-
-
-
-
-
-
+employee_list = [['Robert Stivenson,28', 'Alex Denver,30'], 
+                 ['Drake Mikelsson,19']]
+path = "data.txt" 
+write_employees_to_file(employee_list, path)

5d5072a4e2eab8ada5d3316dd933c282e46ec676
sersim33 <sersim33@gmail.com>
2024-07-14 14:54:21 +0300

    com1

--- 11.py
+++ 11.py
@@ -1,12 +1,9 @@
 # Зчитування двоцифрового числа з клавіатури
 number = int(input("Введіть двоцифрове число: "))
 
-if number >=10:
-    if number <=99:
-        tens = (number // 10) % 10
-        print(tens)
-    else:
-        print("error")
-
+if 10 <= number <= 99:
+    tens = number //10
+    print(tens)
 else:
-    print("need more")
+    print("error")
+
